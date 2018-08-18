9515820e34ab7ecae0b9c0dd96ac9b6e93a2422b
AliaksandraPashko <aliaksandra.pashko@solarwinds.com>
2018-08-18 18:23:55 +0300

    work with widgets

--- main.cpp
+++ main.cpp
@@ -1,25 +1,31 @@
-#include "counter.h"
 #include "mainwindow.h"
 #include <QApplication>
-#include <QLabel>
-#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QLabel lbl("0");
-    QPushButton cmd("Add");
-    Counter counter;
+    QWidget wgt;
+    QWidget* pwgt1 = new QWidget(&wgt);
+    QPalette pal1;
 
-    lbl.show();
-    cmd.show();
+    pal1.setColor(pwgt1->backgroundRole(), Qt::red);
+    pwgt1->setPalette(pal1);
+    pwgt1->resize(100, 100);
+    pwgt1->move(20, 20);
+    pwgt1->setAutoFillBackground(true);
 
-    QObject::connect(&cmd, SIGNAL(clicked()), &counter, SLOT(slotInc()));
+    QWidget* pwgt2 = new QWidget(&wgt);
+    QPalette pal2;
 
-    QObject::connect(&counter, SIGNAL(counterChanged(int)), &lbl, SLOT(setNum(int)));
+    pal2.setColor(pwgt2->backgroundRole(), Qt::blue);
+    pwgt2->setPalette(pal2);
+    pwgt2->resize(100, 100);
+    pwgt2->move(30, 30);
+    pwgt2->setAutoFillBackground(true);
 
-    QObject::connect(&counter, SIGNAL(goodBye()), &app, SLOT(quit()));
+    wgt.resize(200, 200);
+    wgt.show();
 
     return app.exec();
 }
